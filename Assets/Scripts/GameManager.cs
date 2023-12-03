766df56fccb94f3c8c61c8121ced4af660b675f8
JustOlie <oliverdonckers06@gmail.com>
2023-12-03 16:02:33 +0100

    Update GameManager
    Code voor --> vorige commit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager instance;
+
+    [SerializeField] private GameObject _gameOverCanvas;
+
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;   
+        }
+
+        Time.timescale = 1f;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GameOver()
     {
-        
+        _gameOverCanvas.SetActive(true);
+        Time.timescale = 0f;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.Loadscene(SceneManager.GetActiveScene().buildIndex);
     }
 }
