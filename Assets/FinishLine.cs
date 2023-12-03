b75a17274f72dfaeebfeddc5e5f01ec8e4300734
JustOlie <oliverdonckers06@gmail.com>
2023-12-03 21:05:05 +0100

    Scene saves + finishline en terrain updates
    Sommige dingen werken niet

--- Assets/FinishLine.cs
+++ Assets/FinishLine.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        Debug.Log("Trigger entered!");
+
+        if (collision.tag == "Player")
+        {
+            Debug.Log("Player entered!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
